5c31d3eaade4d667e38bc20093b40adca15b5bdf
cph <stegben.benjamin@gmail.com>
2017-10-28 19:06:30 +0800

    0.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='flake8-config-yoctol',
-    version='0.0.2',
+    version='0.0.3',
     url='https://github.com/Yoctol/flake8-config-yoctol',
     author="cph",
     author_email='cph@yoctol.com',
@@ -24,5 +24,13 @@ setup(
     install_requires=[
         'flake8',
         'flake8-bugbear',
+        'flake8-commas',
+        'flake8-import-order',
+        'flake8-builtins',
+        'flake8-class-newline',
+        'flake8-debugger',
+        'flake8-print',
+        'flake8-string-format',
+        'flake8_dodgy'
     ],
 )
