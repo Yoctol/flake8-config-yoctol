0b0f51b5630780f569dba48eb4eb144d6f4b33af
cph <stegben.benjamin@gmail.com>
2018-03-19 16:12:05 +0800

    fix flake8-bugbear cannot install on python3.4 bug

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     keywords='flake8,yoctol',
     install_requires=[
         'flake8==3.5.0',
-        'flake8-bugbear==17.4.0',
+        'flake8-bugbear==17.4.0;python_version>="3.5"',
         'flake8-commas==0.4.3',
         'flake8-builtins==1.0',
         'flake8-class-newline==1.6.0',
